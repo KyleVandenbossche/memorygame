fc0ebfa05f77174ef01ca4de267b688f73b98c6f
Maggie-Wernet <119549545+Maggie-Wernet@users.noreply.github.com>
2023-02-27 19:51:32 -0500

    added start button

--- memorygame.js
+++ memorygame.js
@@ -1,9 +1,9 @@
 //code that runs the count up timer
 
 //timer needs to start (event listener) when user clicks their first card
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
+const minutesLabel = document.getElementById("minutes");
+const secondsLabel = document.getElementById("seconds");
+let totalSeconds = 0;
 setInterval(setTime, 1000);
 
 function setTime() {
@@ -13,12 +13,25 @@ function setTime() {
 }
 
 function pad(val) {
-  var valString = val + "";
+  const valString = val + "";
   if (valString.length < 2) {
     return "0" + valString;
   } else {
     return valString;
   }
 }
+
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", (e) => {
+  started = true;
+  startButton.style.backgroundColor = "white";
+  startButton.style.color = "green";
+  timer.style.backgroundColor = "white";
+  timer.style.color = "green";
+});
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", (e) => {
+  window.location.reload();
+});
 
 //look at Matt's random name code for random cards?
