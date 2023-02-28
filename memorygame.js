c14a3e5355cf43480e9d86afb5f0b3f90761c5e2
Maggie-Wernet <119549545+Maggie-Wernet@users.noreply.github.com>
2023-02-27 20:43:49 -0500

    reset button js and start button connected to timer js

--- memorygame.js
+++ memorygame.js
@@ -1,25 +1,26 @@
 //code that runs the count up timer
 
 //timer needs to start (event listener) when user clicks their first card
-const minutesLabel = document.getElementById("minutes");
-const secondsLabel = document.getElementById("seconds");
-let totalSeconds = 0;
-setInterval(setTime, 1000);
+// const minutesLabel = document.getElementById("minutes");
+// const secondsLabel = document.getElementById("seconds");
+// let totalSeconds = 0;
+// setInterval(setTime, 1000);
 
-function setTime() {
-  ++totalSeconds;
-  secondsLabel.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-}
+// function setTime() {
+//   ++totalSeconds;
+//   secondsLabel.innerHTML = pad(totalSeconds % 60);
+//   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+// }
 
-function pad(val) {
-  const valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
-  }
-}
+// function pad(val) {
+//   const valString = val + "";
+//   if (valString.length < 2) {
+//     return "0" + valString;
+//   } else {
+//     return valString;
+//   }
+// }
+let started = false;
 
 const startButton = document.getElementById("start-button");
 startButton.addEventListener("click", (e) => {
@@ -29,9 +30,23 @@ startButton.addEventListener("click", (e) => {
   timer.style.backgroundColor = "white";
   timer.style.color = "green";
 });
-const resetButton = document.querySelector(".reset");
+const resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener("click", (e) => {
   window.location.reload();
 });
+const timer = document.querySelector("#timer");
 
-//look at Matt's random name code for random cards?
+let timeSeconds = 0;
+let timeMinutes = 0;
+
+const timerFunction = () => {
+  if (started) {
+    timer.textContent = `Timer: ${timeMinutes}:${timeSeconds}`;
+    timeSeconds++;
+    if (timeSeconds === 60) {
+      timeSeconds = 0;
+      timeMinutes++;
+    }
+  }
+};
+const myTimeout = setInterval(timerFunction, 1000);
