b00b4f7f365df2a47f3e9a67c8bf33f11491ca94
Desiree Cushing <desireecushing@gmail.com>
2023-02-27 19:43:44 -0500

    two shuffle function options

--- memorygame.js
+++ memorygame.js
@@ -21,4 +21,27 @@ function pad(val) {
   }
 }
 
+//shuffle function
+// function shuffle() {
+//   cards.forEach(card => {
+//     let shuffledCards = Math.floor(Math.random() * array.length); //what array to use with our code?
+//     card.style.order = shuffledCards;
+//   });
+// }
+
+//jessica's shuffle function
+// function shuffle(cardArray) {
+//   let m = cardArray.length,
+//     t,
+//     i;
+//   while (m) {
+//     i = Math.floor(Math.random() * m--);
+//     t = cardArray[m];
+//     cardArray[m] = cardArray[i];
+//     cardArray[i] = t;
+//   }
+//   return cardArray;
+// }
+
+
 //look at Matt's random name code for random cards?
