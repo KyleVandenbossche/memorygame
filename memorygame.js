594476801bae302c889c1b1b1b2b49b04dbbe47b
Desiree Cushing <desireecushing@gmail.com>
2023-03-04 16:31:46 -0500

    flip back and remove cards working, removed duplicate image

--- memorygame.js
+++ memorygame.js
@@ -8,9 +8,13 @@
   const resetButton = document.querySelector("#reset-button");
 
   // DECLARED VARS
-  let hasFlippedCard = false;
+  //let hasFlippedCard = false;
   let timeSeconds = 0;
   let timeMinutes = 0;
+  let firstCard = null;
+  let secondCard = null;
+  let flippedCards = 0;
+
 
 
   //FUNCTIONS (moved to the top so that we can call when needed)
@@ -21,150 +25,83 @@
     });
   }
 
-
-  // THIS START BUTTON STARTS THE GAME
-  startButton.addEventListener("click", (e) => {
-    // this starts the game, cannot flip cards without pressing the start button
-    started = true;
-    shuffle(); //shuffles the cards when you click start so they don't start in the same order.
-    startButton.style.backgroundColor = "white";
-    startButton.style.color = "green";
-    timer.style.backgroundColor = "white";
-    timer.style.color = "green";
-
-    let firstCard = null;
-    let secondCard = null;
-
-    // THIS IS THE FLIP CARD FUNCTION
-    function flipCard(event) {
-      // this function simply flips the card
-      this.classList.add("flip"); // the classlist.add only flips one way -- toggle flips multiple ways
-      console.log(event.target.dataset.animal);
-
-      if (firstCard === null) {
-        firstCard = event.target; // assigns firstCard as the variable if a div/img is clicked
-      } else {
-        secondCard = event.target; // assigns secondCard as the variable if a div/img is clicked
-
-        if (firstCard.dataset.animal === secondCard.dataset.animal) {
-          // compares the 2 clicks img dataset, if they are the same a MATCH is returned
-          console.log("match");
-
-          //
-          // ONCE THEY MATCH, WE NEED TO KEEP THEM TURNED AND LOOP THROUGH THE FLIP FUNCTION AGAIN
-          //
-        }
-
-        if (firstCard.dataset.animal != secondCard.dataset.animal) {
-          // compares the 2 clicks img dataset, if they are not the same a NO MATCH is returned
-          console.log("no match");
-
-          //
-          // IF THEY DO NOT MATCH, WE NEED TO FLIP THEM OVER AGAIN, AND LOOP THROUGH THE FLIP FUNCTION UNTIL THEY MATCH
-          //
-        }
-
-        firstCard = null;
-        secondCard = null;
-      }
-    }
-    cards.forEach((card) => card.addEventListener("click", flipCard));
-
-    // checkForMatch();
-
-    // function checkForMatch(){
-
-    //   firstCard = null; // assigning no value to firstCard
-    //   secondCard = null; // assigning no value to secondCard
-
-    //   const cards = document.querySelectorAll(".memory-card");
-    //   cards.addEventListener("click", (event) => {
-
-    //     if (firstCard === null){
-    //      firstCard = event.target; // event.target (if the event happens of clicking) -- it assigns the click as firstCard
-    //      } else {
-    //      secondCard = event.target; // event.target (if the event happens of clicking) -- it assigns the click as secondCard
-
-    //      if (firstCard.target.dataset.animal === secondCard.target.dataset.animal){
-    //       console.log('MATCH');
-    //      }
-
-    // }})
-
-    // }
-
-    //   if (!hasFlippedCard) { // if nothing happens nothing is assigned
-    //     hasFlippedCard = true;  // if this happens, continue
-    //     firstCard = this; // the first card is -- this variable represents the card clicked
-    //     console.log('hello');
-    //    return;
-    //   }
-
-    // secondCard = this;
-    // hasFlippedCard = false;
-
-    // function checkForMatch() {
-    // if (firstCard.dataset.animal === secondCard.dataset.animal) {
-    //   alert("MATCH");
-    //   return;
-    // }
-    // }
-
-    // checkForMatch(firstCard, secondCard);
-  });
-
-  resetButton.addEventListener("click", (e) => {
-    window.location.reload();
-    shuffle();
-  });
-
-
-
-  //   if (!hasFlippedCard) {
-  //     hasFlippedCard = true;
-  //     firstCard = this;
-  //    return;
-  //   }
-  // }
-  //  cards.forEach((card) => card.addEventListener("click", flipCard));
-
-  // secondCard = this;
-  // hasFlippedCard = false;
-
-  // function checkForMatch() {
-  // if (firstCard.data.animal === secondCard.data.animal) {
-  //   disableCards();
-  //   console.log(`checkForMatch`)
-  //   return;
-  // }
-
-  // unflipCards();
-  // }
-
-  // checkForMatch();
-
-  // function disableCards() {
-  // firstCard.removeEventListener('click', flipCard);
-  // secondCard.removeEventListener('click', flipCard);
-  // }
-
-  // function unflipCards() {
-  // setTimeout(() => {
-  //   firstCard.classList.remove('flip');
-  //   secondCard.classList.remove('flip');
-  // }, 1500);
-  // }
-  // });
   const timerFunction = () => {
     if (started) {
       timer.textContent = `Timer: ${timeMinutes}:${timeSeconds}`;
       timeSeconds++;
+
+      startButton.style.backgroundColor = "white";
+      startButton.style.color = "green";
+      timer.style.backgroundColor = "white";
+      timer.style.color = "green";
+
       if (timeSeconds === 60) {
         timeSeconds = 0;
         timeMinutes++;
       }
     }
   };
+
+  function unFlipCards(firstCard, secondCard) {
+    console.log(firstCard, secondCard) //don't need line 46 in finished
+    setTimeout(() => {
+      firstCard.parentNode.classList.remove('flip');
+      secondCard.parentNode.classList.remove('flip');
+    }, 1000);
+  }
+
+  function flipCard(event) {
+    // this function simply flips the card
+
+    //CHANGE EVENT TARGETS TO TARGET EVENTS PARENT NODE
+    //event.target.classList.add("flip"); // the classlist.add only flips one way -- toggle flips multiple ways
+    event.target.parentNode.classList.add('flip');
+    console.log(event.target.dataset.animal);
+
+    if (firstCard === null) {
+      firstCard = event.target; // assigns firstCard as the variable if a div/img is clicked
+    } else {
+      secondCard = event.target; // assigns secondCard as the variable if a div/img is clicked
+
+      if (firstCard.dataset.animal === secondCard.dataset.animal) {
+        // compares the 2 clicks img dataset, if they are the same a MATCH is returned
+        console.log("match");
+
+        firstCard.parentNode.style.opacity = 0;
+        secondCard.parentNode.style.opacity = 0;
+
+
+        //CARD OPACITY SPEED TOO FAST  -- OPACITY VS VISIBILITY?
+      } else {
+        console.log("no match");
+        unFlipCards(firstCard, secondCard);
+      }
+
+      firstCard = null;
+      secondCard = null;
+    }
+  }
+
+
+
+
+  // THIS START BUTTON STARTS THE GAME
+  startButton.addEventListener("click", (event) => {
+    // this starts the game, cannot flip cards without pressing the start button
+    started = true;
+    timerFunction();
+    shuffle(); //shuffles the cards when you click start so they don't start in the same order.
+    cards.forEach((card) => card.addEventListener("click", (cardClick) => {
+      flipCard(cardClick);
+    }))
+  });
+
+  resetButton.addEventListener("click", (e) => {
+    window.location.reload(e);
+    shuffle();
+  });
+
+
   const myTimeout = setInterval(timerFunction, 500); // Needs to be finished, duration of flip time -- if it matches it keeps it on the screen for a certain time -- Jessica
 
 
