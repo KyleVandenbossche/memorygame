83c79cba2f53d0f2eafc70eb5f302ee688b7940f
Desiree Cushing <desireecushing@gmail.com>
2023-03-05 18:18:22 -0500

    cleaned up code and comments

--- memorygame.js
+++ memorygame.js
@@ -1,23 +1,25 @@
 (() => {
-  let started = false; // timer starts at off -- Maggie
+
+  // DECLARED VARS
+  let started = false; //timer starts when true
+  let timeSeconds = 0;
+  let timeMinutes = 0;
+
+  let firstCard = null; //allows user to choose first/second cards
+  let secondCard = null;
+
 
   // HTML CALLS
+
   const cards = document.querySelectorAll(".memory-card"); // calling the memory card html
   const startButton = document.getElementById("start-button"); // calling the start button
   const timer = document.querySelector("#timer");
   const resetButton = document.querySelector("#reset-button");
 
-  // DECLARED VARS
-  //let hasFlippedCard = false;
-  let timeSeconds = 0;
-  let timeMinutes = 0;
-  let firstCard = null;
-  let secondCard = null;
-  let flippedCards = 0;
 
 
+  //FUNCTIONS
 
-  //FUNCTIONS (moved to the top so that we can call when needed)
   function shuffle() {
     cards.forEach((card) => {
       let randomPos = Math.floor(Math.random() * 16);
@@ -43,18 +45,18 @@
   };
 
   function unFlipCards(firstCard, secondCard) {
-    console.log(firstCard, secondCard) //don't need line 46 in finished
     setTimeout(() => {
-      firstCard.parentNode.classList.remove('flip');
+      firstCard.parentNode.classList.remove('flip'); //removing class, not card
       secondCard.parentNode.classList.remove('flip');
-    }, 1000);
+    }, 3000);
+  }
+
+  function gotMatch(firstCard, secondCard) {
+    firstCard.parentNode.style.opacity = 0.5; //instead of adding class, we have applied style here
+    secondCard.parentNode.style.opacity = 0.5;
   }
 
   function flipCard(event) {
-    // this function simply flips the card
-
-    //CHANGE EVENT TARGETS TO TARGET EVENTS PARENT NODE
-    //event.target.classList.add("flip"); // the classlist.add only flips one way -- toggle flips multiple ways
     event.target.parentNode.classList.add('flip');
     console.log(event.target.dataset.animal);
 
@@ -65,13 +67,9 @@
 
       if (firstCard.dataset.animal === secondCard.dataset.animal) {
         // compares the 2 clicks img dataset, if they are the same a MATCH is returned
-        console.log("match");
+        //console.log("match");
+        gotMatch(firstCard, secondCard);
 
-        firstCard.parentNode.style.opacity = 0;
-        secondCard.parentNode.style.opacity = 0;
-
-
-        //CARD OPACITY SPEED TOO FAST  -- OPACITY VS VISIBILITY?
       } else {
         console.log("no match");
         unFlipCards(firstCard, secondCard);
@@ -87,7 +85,7 @@
 
   // THIS START BUTTON STARTS THE GAME
   startButton.addEventListener("click", (event) => {
-    // this starts the game, cannot flip cards without pressing the start button
+    //cannot flip cards until start button pressed
     started = true;
     timerFunction();
     shuffle(); //shuffles the cards when you click start so they don't start in the same order.
